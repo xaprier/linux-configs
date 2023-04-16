5d93cb2f0c97f24bdc0548dfc9118366b0bc18bd
Seymen Kalkan <seymenkalkan@gmail.com>
2023-04-16 14:56:27 +0300

    Update configs
    Changing "/home/xxx -> ~" for effecting which user running

--- i3/polybar/scripts/shutdown.sh
+++ i3/polybar/scripts/shutdown.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-chosen=$(echo -e "     \n     \n    \n    " | rofi -dmenu -config /home/xaprier/.config/rofi/power.rasi -lines 5 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Power Menu")
+chosen=$(echo -e "     \n     \n    \n    " | rofi -dmenu -config ~/.config/rofi/power.rasi -lines 5 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Power Menu")
 
 if [[ $chosen = "     " ]]; then
-  ui=$(echo -e "           \n    " | rofi -dmenu -config /home/xaprier/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Logout ?")
+  ui=$(echo -e "           \n    " | rofi -dmenu -config ~/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Logout ?")
   if [[ $ui = "           " ]]; then
   i3-msg exit
 fi
 elif [[ $chosen = "    " ]]; then
-  ui=$(echo -e "           \n    " | rofi -dmenu -config /home/xaprier/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Shutdown ?")
+  ui=$(echo -e "           \n    " | rofi -dmenu -config ~/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Shutdown ?")
   if [[ $ui = "           " ]]; then
   systemctl poweroff
 fi
 elif [[ $chosen = "    " ]]; then
-  ui=$(echo -e "           \n    " | rofi -dmenu -config /home/xaprier/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Reboot ?")
+  ui=$(echo -e "           \n    " | rofi -dmenu -config ~/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Reboot ?")
   if [[ $ui = "           " ]]; then
   systemctl reboot
 fi	
 elif [[ $chosen = "     " ]]; then
-  ui=$(echo -e "           \n    " | rofi -dmenu -config /home/xaprier/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Suspend ?")
+  ui=$(echo -e "           \n    " | rofi -dmenu -config ~/.config/rofi/power.rasi -lines 2 -eh 2 -padding 850 -font "SF Pro Display 18" -p "                               Suspend ?")
   if [[ $ui = "           " ]]; then
   betterlockscreen -l dim  & systemctl suspend
 fi	
