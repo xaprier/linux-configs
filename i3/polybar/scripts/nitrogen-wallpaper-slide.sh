c08f0b244aa03cd33abe5656e32e422f05c598eb
Seymen Kalkan <seymenkalkan@gmail.com>
2023-04-19 19:43:08 +0300

    Update i3 nitrogen-wallpaper script
    A parameter removed

--- i3/polybar/scripts/nitrogen-wallpaper-slide.sh
+++ i3/polybar/scripts/nitrogen-wallpaper-slide.sh
@@ -6,4 +6,4 @@ WALLPAPERS="$HOME/Pictures/wallpaper/"
 ALIST=( `ls -w1 $WALLPAPERS` )
 RANGE=${#ALIST[@]}
 let "number = $RANDOM % $RANGE"
-nitrogen --set-scaled --save $WALLPAPERS/${ALIST[$number]}
+nitrogen --save $WALLPAPERS/${ALIST[$number]}
