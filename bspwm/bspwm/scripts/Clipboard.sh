1d54ffa7fa0d17b58fdd0b34bb6942f3238a9ae6
Seymen Kalkan <seymenkalkan@gmail.com>
2024-11-01 16:38:47 +0300

    Updated bspwm and added synaptics

--- bspwm/bspwm/scripts/Clipboard.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$HOME/.config/bspwm"
-RASI="$DIR/scripts/Screenshot.rasi"
-
-# Options
-rofi -modi "clipboard:greenclip print" -show clipboard -run-command '{cmd}' -theme ${RASI}
